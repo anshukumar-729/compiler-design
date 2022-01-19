50cee71161c1db6915c3a474b5395c8e272829a9
Anshu kumar <anshukumar93.40@gmail.com>
2022-01-19 16:34:13 +0530

    commit

--- Fortran files/add.f90
+++ Fortran files/add.f90
@@ -5,13 +5,15 @@ program addNumbers
 
 ! Type declarations
    integer :: a, b, result
+   character(len=10) :: f
 
 ! Executable statements
+   f="$5%"
    a = 9
    b = 1
    if (a<b)then
    print *, 'The total is ', result
    else
-      print *, 0
+      print *, f
    end if
 end program addNumbers
