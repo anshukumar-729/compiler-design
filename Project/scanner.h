d60d1885ee4c0aa7012ebacc50cb84d276864d61
Nancy <guptanancy2403@gmail.com>
2022-01-19 22:47:21 +0530

    nancy

--- Project/scanner.h
+++ Project/scanner.h
@@ -12,4 +12,22 @@
 #define op_DIV 12 
 #define op_EQUALITY 13 
 #define op_RELATIONAL 14 
+#define kw_IF 15
+#define kw_ELSE 16
+#define kw_END 17
+#define kw_THEN 18
+#define kw_TYPE 19
+#define dt_INT 20
+#define dt_COMPLEX 21
+#define dt_LOGICALS 22
+#define dt_CHARACTER 23
+#define sp_LB 24
+#define sp_RB 25
+#define sp_CURLYLB 26
+#define sp_CURLYRB 27
+#define sp_SQUARELB 28
+#define sp_SQUARERB 29
+#define nl 30
+
+
 
